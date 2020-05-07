355de14afb52ca7f196b92b572527c6dfdc7af83
João Emanuel <joaoemanuel.no@gmail.com>
2020-05-07 15:03:06 -0400

    Projeto incial, exbindo mensagens.

---
+++ Projeto-teste/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("¡Hola Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
